2ff71c934b617d5eadb423aafdb27f9e5dba9f1d
hcgcarry <djjjimmyyy@gmail.com>
2020-08-25 14:02:25 +0800

    modify netToOnnx.py

--- netToOnnx.py
+++ netToOnnx.py
@@ -52,7 +52,7 @@ x = torch.randn(batch_size, 3, 224, 224, requires_grad=True)
 # Export the model
 torch.onnx.export(net,               # model being run
                   x,                         # model input (or a tuple for multiple inputs)
-                  "./trainedModel/super_resolution.onnx",   # where to save the model (can be a file or file-like object)
+                  "./trainedModel/model_batchsize_"+str(batch_size)+".onnx",   # where to save the model (can be a file or file-like object)
                   export_params=True,        # store the trained parameter weights inside the model file
                   opset_version=10,          # the ONNX version to export the model to
                   do_constant_folding=True,  # whether to execute constant folding for optimization
